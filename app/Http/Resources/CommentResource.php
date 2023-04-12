5f69dabb5241c4968892fe9f50d1c868cce6c069
Sarawut Oppara <sarawut.op@ku.th>
2023-04-13 00:20:59 +0700

    add id key for comment id in commentresource

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -15,6 +15,7 @@ class CommentResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'user' => new UserResource($this->user),
             'message' => $this->message,
             'created_at' => $this->created_at,
